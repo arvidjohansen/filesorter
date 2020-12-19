b40680686595b41519d5ddf972e32644c6477fc7
Biskus <biskus@gmail.com>
2020-12-19 19:09:24 +0100

    Adjustments made

--- filesorter.py
+++ filesorter.py
@@ -1,9 +1,7 @@
 
 import os
-
-from pprint import pprint as p 
-
-p(dir(os))
+from collections import defaultdict 
+from pprint import pprint as p
 
 extensions = [
     'csv',
@@ -28,24 +26,41 @@ extensions = [
     'xls',
     'xlsx',
     'zip',
-
 ]
 
-p(os.listdir())
+count_dict = defaultdict(int)
+def create_directories():
+    files = os.listdir()
+    for f in files:
+        for e in extensions:
+            if f.endswith('.' + e):
+                count_dict[e] += 1
+    for ext, count in count_dict.items():
+        if count > 0:
+            try:
+                os.mkdir(ext)
+                print("Created directory for %s" % ext)
+            except FileExistsError:
+                print("Directory %s already exists, skipping" % ext)
+create_directories()
+
+
+
+p(count_dict)
+
 
 files = os.listdir()
 
-for ext in extensions:
-    try:
-        os.mkdir(ext)
-        print("Created directory for %s" % ext)
-    except FileExistsError:
-        print("Directory %s already exists, skipping" % ext)
 
+def move_files():
+    for f in files:
+        for e in extensions:
+            if f.endswith('.'+e):
+                os.rename(f,e+'/'+f)
+                print("Successfully moved file %s to directory %s" % ((f,e)))
 
-for f in files:
-    for e in extensions:
-        #print("File %s ends with %s" % ((f,e)))
-        if f.endswith('.'+e):
-            os.rename(f,e+'/'+f)
-            print("Successfully moved file %s to directory %s" % ((f,e)))
+ans = input("Moving files into directories. Continue? (y/n)\n")
+if ans.lower() == 'y':
+    move_files()
+else:
+    print("Exiting...")
